90771ab432845aff69f20151db721dc03826a738
Jang <bigonej3@naver.com>
2024-07-04 19:58:48 +0900

    antd 수정

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import logo2 from '../../images/logo2.png'
 import advertise from '../../images/advertise.png'
-import { Button, Input, Select } from 'antd';
+import { Button, Input} from 'antd';
 import { Link } from 'react-router-dom';
+import { InputNumber, Space } from 'antd';
+
+
 
 function LandingPage() {
-  const handleChange = (value) => {
-    console.log(`selected ${value}`);
+  const onChange = (value) => {
+    console.log('changed', value);
   };
-  const options = [];
+
   return (
 
     <div>
@@ -17,25 +20,22 @@ function LandingPage() {
           <img src={logo2} />
         </div>
       </Link>
-      {/* <div style={{
-        display: 'flex',
-        alignItems: 'center',
-        margin: '0, auto',
-        height: '100px',
-        flexDirection : 'row',
-        justifyContent: 'center', 
-      }}> */}
+
       <div style={{ textAlign: "center", margin: '0, auto' }}>
         <Input placeholder='검색 : 플레이어 닉네임' style={{ width: '600px', marginRight: '10px' }} />
         <Button htmlType="submit" style={{ border: ' solid #00bfff', color: '#00bfff', fontWeight: 'bold' }}>조회</Button>
       </div>
-      <div style={{ textAlign: "center", margin: '30px auto 0 -70px'}}>
-        <Select
-          mode="tags"
-          onChange={handleChange}
-          tokenSeparators={[',']}
-          options={options}
-          style={{ width: '600px', margin: '0 auto' }}/>
+      <div style={{ textAlign: "center", margin: '30px auto 0 -70px' }}>
+        <Space>
+          <InputNumber
+            defaultValue={'인기 커뮤니티 글'}
+            formatter={(value) => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+            parser={(value) => value?.replace(/\$\s?|(,*)/g, '')}
+            onChange={onChange}
+            style={{width: '600px'}}
+          />
+          </Space>
+   
       </div>
       <div style={{ textAlign: "center", margin: '100px auto 0 auto' }}>
         <img src={advertise} />
