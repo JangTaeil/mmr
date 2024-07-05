1c3bdfc4a099971fdab29a0f907f150a09e54bf7
Jang <bigonej3@naver.com>
2024-07-05 16:47:40 +0900

    랭킹페이지 이미지 구현, 챔피언 이름 가져오기

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo2 from '../../images/logo2.png'
 import advertise from '../../images/advertise.png'
 import { Button, Input } from 'antd';
@@ -8,9 +8,34 @@ import { InputNumber, Space } from 'antd';
 
 
 function LandingPage() {
-  // const onChange = (value) => {
-  //   console.log('changed', value);
-  // };
+
+  const [displayText, setDisplayText] = useState('인기 커뮤니티 글');
+  const [currentValue, setCurrentValue] = useState(1);
+
+  // 증가 버튼 핸들러
+  const handleIncrease = () => {
+    if (currentValue < 3) {
+      setCurrentValue(currentValue + 1);
+    }
+    updateDisplayText(currentValue + 1);
+  };
+
+  // 감소 버튼 핸들러
+  const handleDecrease = () => {
+    if (currentValue > 1) {
+      setCurrentValue(currentValue - 1);
+    }
+    updateDisplayText(currentValue - 1);
+  };
+
+  const updateDisplayText = (value) => {
+    if (value === 1) {
+      setDisplayText('인기 커뮤니티 글');
+    } else if (value === 2) {
+      setDisplayText('베스트 인기글');
+    }
+  };
+
 
   return (
 
@@ -25,19 +50,14 @@ function LandingPage() {
         <Input placeholder='검색 : 플레이어 닉네임' style={{ width: '600px', marginRight: '10px' }} />
         <Button htmlType="submit" style={{ border: ' solid #00bfff', color: '#00bfff', fontWeight: 'bold' }}>조회</Button>
       </div>
-      <div style={{ textAlign: "center", margin: '30px auto 0 -70px' }}>
-        <Space>
-          <InputNumber
-            min={1}
-            max={1}
-            defaultValue={'인기 커뮤니티 글'}
-            style={{ width: '600px' }}
-          />
-        </Space>
-
+      <div style={{ textAlign: "center", margin: '30px auto' }}>
+        <Input value={displayText} style={{ width: '600px' }} readOnly />
+        <Button onClick={handleIncrease} style={{ margin: '10px' }}>🔼</Button>
+        <Button onClick={handleDecrease}>🔽</Button>
       </div>
+
       <div style={{ textAlign: "center", margin: '100px auto 0 auto' }}>
-        <img src={advertise} />
+        <img src={advertise} alt="Advertisement" />
       </div>
     </div>
 
--- client/src/components/Ranking/Ranking.js
+++ client/src/components/Ranking/Ranking.js
@@ -1,48 +1,45 @@
-import { Button, Input } from 'antd';
 import React from 'react';
-import alldata from '../../data/alldata.json';
+import { Button, Input } from 'antd';
 import advertise2 from '../../images/advertise2.png';
 import advertise3 from '../../images/advertise3.PNG';
+import alldata from '../../data/alldata.json';
 import { IMAGE_BASE_URL } from '../Config';
-import { Card } from 'antd';
-
-const { Meta } = Card;  
 
 const Ranking = () => {
-
-  const data_list = Object.values(alldata.data);
+  // 객체에서 배열 형태로 변환
+  const data_list = Object.values(alldata.data); 
 
   return (
     <div>
       <div style={{ margin: '50px auto', textAlign: 'center' }}>
         <Input placeholder='검색 : 플레이어 닉네임' style={{ width: '800px', height: '4rem' }} />
-        <Button htmlType="submit" style={{ marginLeft: '10px', border: ' solid #00bfff', color: '#00bfff', fontWeight: 'bold', padding: '15px 15px' }}>조회</Button>
+        <Button htmlType="submit" style={{ marginLeft: '10px', border: 'solid #00bfff', color: '#00bfff', fontWeight: 'bold', padding: '15px 15px' }}>조회</Button>
       </div>
 
-      <div style={{ display: 'flex' }}>
-        <div>
-          <img src={advertise2}
-            style={{ margin: '170px 0 0 30px', width: '300px' }} />
+      <div style={{ display: 'flex', alignItems: 'flex-start' }}>
+        {/* 좌측 광고 이미지 */}
+        <div style={{ marginRight: '20px' }}>
+          <img src={advertise2} alt="좌측 광고 이미지" style={{ width: '300px' }} />
         </div>
 
-        <div style={{ width: '1500px', display: 'flex', flexWrap: 'wrap' }}>
-        {data_list.map((champion, index) => (
-             <Card
-             hoverable
-             style={{
-               width: 200,
-             }}
-             cover={<img key={index} src={`${IMAGE_BASE_URL}/${champion.image.full}`} alt={`챔피언 ${champion.name} 이미지`} style={{ width: '48px', height: '48px', margin: '3px' }} /> }>
-              <Meta title={`${champion.name}`}/>
-             </Card>
+        <div style={{ width: 'calc(100% - 620px)', display: 'flex', flexWrap: 'wrap', justifyContent: 'flex-start' }}>
+          {data_list.map((champion, index) => (
+            <div>
+              <div key={index} style={{ display: 'flex', alignItems: 'center', margin: '5px', width: '100px' }}>
+                <img src={`${IMAGE_BASE_URL}/${champion.image.full}`} alt={`챔피언 ${champion.name} 이미지`} style={{ width: '48px', height: '48px', marginRight: '10px' }} />
+              </div>
+              <span>{champion.name}</span>
+            </div>
           ))}
         </div>
-        <div style={{ marginRight: "170px" }}>
-          <img src={advertise3} style={{ width: '200%' }} />
+
+        {/* 우측 광고 이미지 */}
+        <div style={{ marginLeft: 'auto' }}>
+          <img src={advertise3} alt="우측 광고 이미지" style={{ width: '300px' }} />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Ranking
+export default Ranking;
